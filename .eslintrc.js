1d40a69d6ce48756f698709cd61e99d051a9321d
Vaibhavbairagi <vaibhavbairagi105@gmail.com>
2020-03-09 18:05:10 +0530

    modified some files

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+  module.exports = {
+      "root": true,
+      "extends": "standard",
+      "rules": {
+          "semi": ["error", "always"],
+          "indent": ["error", 2],
+          "space-before-function-paren": ["error", "never"],
+          "no-trailing-spaces": ["error", {
+              "skipBlankLines": true
+          }],
+          "no-multiple-empty-lines": ["error", {
+              "max": 4,
+              "maxEOF": 2
+          }],
+      }
+  };
